0296dceab19f03ab5e7b5783fc6bab4ccdc2370f
max anselmo <itmanselmo@gmail.com>
2021-09-20 11:54:08 -0400

    commit tree a39d27c6421169099b44ad5924a377a6713b3a0b

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideHeight){
   // Write your code here!
+  if(rideHeight <= 400){
+    return 'This one is on me!';
+  } 
+  else if(rideHeight > 2500){
+    return 'No can do.';
+  }
+  else if(rideHeight > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(isGenerous){
+  if(isGenerous === 'generous'){
+    return 'Thank you so much.';
+  }
+  else if(isGenerous === 'not as generous'){
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
